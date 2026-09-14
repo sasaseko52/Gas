d1df59bc47b43f59815f16cf69998a9c24e91e67
Mostafa <sasaseko52@gmail.com>
2026-09-15 01:37:05 +0300

    Frozen and goblin axr bp creation and adding weapon (SKMs)

--- Source/Aura/Private/Character/MainCharacterBase.cpp
+++ Source/Aura/Private/Character/MainCharacterBase.cpp
@@ -3,14 +3,15 @@
 
 #include "Character/MainCharacterBase.h"
 
-#include "Kismet/GameplayStatics.h"
 
 
 AMainCharacterBase::AMainCharacterBase()
 {
  	PrimaryActorTick.bCanEverTick = false;
 	
-	
+	Weapon  = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
