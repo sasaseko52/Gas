3fa764458abbca134d8f92758bdc2249be17b679
Mostafa <sasaseko52@gmail.com>
2026-09-23 01:19:16 +0300

    Create GameplayAttributeData(Health,MxHealth,Mana, MaxMana) , Adding Attribute Accessors and Intialzing Attributes, Test with showdebug abilitysystem

--- Source/Aura/Private/AbilitySystem/FrozenAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/FrozenAttributeSet.cpp
@@ -2,4 +2,44 @@
 
 
 #include "AbilitySystem/FrozenAttributeSet.h"
+#include"AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UFrozenAttributeSet::UFrozenAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(100.f);
+	InitMaxMana(100.f);
+}
+
+void UFrozenAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(UFrozenAttributeSet,Health,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UFrozenAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UFrozenAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UFrozenAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+}
+
+void UFrozenAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UFrozenAttributeSet,Health, OldHealth);
+	
+}
+
+void UFrozenAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UFrozenAttributeSet,MaxHealth, OldMaxHealth);
+}
+
+void UFrozenAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UFrozenAttributeSet,Mana, OldMana);
+}
+
+void UFrozenAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UFrozenAttributeSet,MaxMana, OldMaxMana);
+}
--- Source/Aura/Public/AbilitySystem/FrozenAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/FrozenAttributeSet.h
@@ -4,14 +4,49 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "FrozenAttributeSet.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class AURA_API UFrozenAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	UFrozenAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 	
+	///Attributes
+	///Health
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Health, Category="Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UFrozenAttributeSet,Health);
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth)const;
+	
+	///MaxHealth
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_MaxHealth, Category="Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UFrozenAttributeSet,MaxHealth);
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)const;
+	
+	///Mana
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing= OnRep_Mana,Category="Vital Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UFrozenAttributeSet,Mana);
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana)const;
+	
+	///MaxMana
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxMana,Category="Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UFrozenAttributeSet,MaxMana);
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)const;
 };
